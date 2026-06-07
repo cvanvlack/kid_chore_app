944570144f76a2d6916be08fd6aff93805931c1a
Cole Van Vlack <cvanvlack@gmail.com>
2026-06-07 15:13:56 -0400

    Updated to new endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   API_URL:
-    'https://script.google.com/macros/s/AKfycbzaFXY09GiuxI9MEefVqriWPFR5poe40-_Nhlgyg47flEdpwlC9nRqJPSn__Td_pxO2/exec',
+    'https://script.google.com/macros/s/AKfycbww9QeWZcok7kKD7I6_fIU37jLDPjmN0O9Sb4ZqyDg4gv8E-_f7BBGP4x9cx0tShi8Uyg/exec',
   KIDS: [
     { id: 'k1', name: 'Kid 1' },
     { id: 'k2', name: 'Kid 2' },
